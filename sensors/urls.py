5c0a1b8eaeb2a963349abe86ae6e40c4ad2e7e43
estri012 <estri.puji.lestari@mail.ugm.ac.id>
2020-05-08 13:31:26 +0700

    renew chart sensor

--- sensors/urls.py
+++ sensors/urls.py
@@ -2,16 +2,8 @@ from django.urls import path
 from . import views
 from restapi import views
 from restapi.models import Data, Node
-from sensors.views import SoilMoisture, Vibration, Displacement, AcceleroX, AcceleroY, AcceleroZ, GyroX, GyroY, GyroZ
+from sensors.views import SensorNode1
 
 urlpatterns = [
-    path('soil-moisture/', SoilMoisture.as_view(), name='soilmoisture'),
-    path('vibration/', Vibration.as_view(), name="vibration"),
-    path('displacement/', Displacement.as_view(), name="displacement"),
-    path('accelero-x/', AcceleroX.as_view(), name="accelero-x"),
-    path('accelero-y/', AcceleroY.as_view(), name="accelero-y"),
-    path('accelero-z/', AcceleroZ.as_view(), name="accelero-z"),
-    path('gyro-x/', GyroX.as_view(), name="gyro-x"),
-    path('gyro-y/', GyroY.as_view(), name="gyro-y"),
-    path('gyro-z/', GyroZ.as_view(), name="gyro-z"),
+    path('node1/', SensorNode1.as_view(), name='node1'),
 ]
--- sensors/views.py
+++ sensors/views.py
@@ -9,38 +9,6 @@ from rest_framework import routers, serializers, viewsets
 from rest_framework.response import Response
 import json
 # Create your views here.
-class SoilMoisture(View):
+class SensorNode1(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'soilmoisture.html')
-
-class Vibration(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'vibration.html')
-
-class Displacement(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'displacement.html')
-
-class AcceleroX(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'accelero-x.html')
-
-class AcceleroY(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'accelero-y.html')
-
-class AcceleroZ(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'accelero-z.html')
-
-class GyroX(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'gyro-x.html')
-
-class GyroY(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'gyro-y.html')
-
-class GyroZ(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'gyro-z.html')
+        return render(request, 'sensor_n1.html')
